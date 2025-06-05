a7ae121e1d0af37932c95667f22e459484f625e4
Andrew Lee Rubinger <alr@alrubinger.com>
2025-06-04 20:16:59 -0700

    Fix GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://alrubinger.github.io',
   base: '/mic-check',
+  output: 'static',
+  outDir: './dist',
 });
